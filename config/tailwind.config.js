bf64ec2e5e4eb220d52a9a0a71b67de21acca5f9
Nils Duesterhoeft <nilsd@me.com>
2023-08-25 18:56:09 +0200

    Finalized mobile header

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    fontSize: {
+      xs: '0.75rem',
+      '2xs' : '0.65rem',
+      sm: '0.875rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
